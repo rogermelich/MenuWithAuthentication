6dd36b43bdd9fe01dd85b6b7319256cf1722248d
rogermelich <rogermelich@gmail.com>
2015-12-14 16:55:15 +0100

    Modified menu.php, to add configuration

--- app/bootstrap/menu.php
+++ app/bootstrap/menu.php
@@ -7,12 +7,26 @@
 
 
 //Home Menu
-MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title('Home')
+    ->icon('fa-dashboard')
+    ->url('/home')
+    ->rol('home')
+    ->permission('home')
+    ->user('rogermelich');
 
 //Another link Menu
-MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title('Another Link')
+    ->user(5);
 
 //Multilevel Menu
-MenuWithAuthentication::menu
-    MenuWithAuthentication::menu
-    MenuWithAuthentication::menu
+MenuWithAuthentication::menu()->title('Multilevel')->icon('fa-clone');
+    MenuWithAuthentication::menu()->title('Multilevel 1')->icon('fa-hand-peace-o');
+    MenuWithAuthentication::menu()->title('Multilevel 2')->icon('fa-hand-peace-o');
+
+//Add últim menu to difrence to origin configuration
+MenuWithAuthentication::menu()
+    ->title('Últim Fitxer')
+    ->icon('fa-camera')
+    ->url('http://www.google.com');
