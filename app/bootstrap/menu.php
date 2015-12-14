b9934692241bcd99686226255e805e6e1ea8020a
rogermelich <rogermelich@gmail.com>
2015-12-14 16:38:24 +0100

    Create Dir and menu.php into app

---
+++ app/bootstrap/menu.php
@@ -0,0 +1,18 @@
+<?php
+
+//Name  or title
+//URL links
+//Icons
+//It's Multilevel Menu
+
+
+//Home Menu
+MenuWithAuthentication::menu
+
+//Another link Menu
+MenuWithAuthentication::menu
+
+//Multilevel Menu
+MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
